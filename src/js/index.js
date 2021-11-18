10639690d8571e5e58edac4e607cdc7681feb1e5
서지원 <qrd7079@gmail.com>
2021-11-18 16:54:30 +0900

    메뉴 탭별 관리

--- src/js/index.js
+++ src/js/index.js
@@ -29,27 +29,26 @@
 // TODO localStorage Read & Write
 // localStorage에 데이터를 저장한다.
 // [o] 메뉴를 추가할 때
-// [] 메뉴를 수정할 때
-// [] 메뉴를 삭제할 때
-// [ ] localStorage에 있는 데이터를 읽어온다.
+// [o] 메뉴를 수정할 때
+// [o] 메뉴를 삭제할 때
+// [o] localStorage에 있는 데이터를 읽어온다.
 /* 
 localStorage.setItem("menu","espresso") 
 -> 로컬스토리지에 데이터 저장(키, 값)
-
 localStorage.getItem("menu") -> 로컬스토리지에서 데이터 가져오기(키)
 'espresso' 
 */
 
 // TODO 카테고리별 메뉴판 관리
-// [ ] 에스프레소 메뉴판 관리
-// [ ] 프라푸치노 메뉴판 관리
-// [ ] 블렌디드 메뉴판 관리
-// [ ] 티바나 메뉴판 관리
-// [ ] 디저트 메뉴판 관리
+// [o] 에스프레소 메뉴판 관리
+// [o] 프라푸치노 메뉴판 관리
+// [o] 블렌디드 메뉴판 관리
+// [o] 티바나 메뉴판 관리
+// [o] 디저트 메뉴판 관리
 
 // TODO 페이지 접근시 최초 데이터 Read & Rendering
-// [ ] 페이지가 최초로 로딩될때 localStorage 에서 에스프레소 메뉴를 읽어온다.
-// [ ] 에스프레소 메뉴를 페이지에 그려준다.
+// [o] 페이지가 최초로 로딩될때 localStorage 에서 에스프레소 메뉴를 읽어온다.
+// [o] 에스프레소 메뉴를 페이지에 그려준다.
 
 // TODO 품절 상태 관리
 // [ ] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
@@ -68,7 +67,8 @@ const store = {
     // 문자열로만 저장이 되어야해서 JSON.stringify(menu)를 활용
   },
   getLocalStorage() {
-    localStorage.getItem("menu");
+    return JSON.parse(localStorage.getItem("menu"));
+    // 문자를 다시 JSON객체로 저장
   },
 };
 // 로컬스토리지 확인하는 방법 -> 개발자도구 - 애플리케이션 - 로컬스토리지
@@ -76,65 +76,90 @@ const store = {
 function App() {
   // 상태는 변하는 데이터, 이 앱에서 변하는 것이 무엇인가 - 메뉴명
   // 갯수는 업데이트만 되면 되지 메뉴명의 길이로 구하면 되서 관리할 대상 아님.
-  this.menu = []; // -> 상태값선언, 초기화
+  this.menu = {
+    espresso: [],
+    frappuccino: [],
+    blended: [],
+    teavana: [],
+    desert: [],
+  }; // -> 상태값선언, 초기화
+  this.currentCategory = "espresso"; // 현재카테고리(기본값 espresso)
 
+  this.init = () => {
+    if (store.getLocalStorage()) {
+      this.menu = store.getLocalStorage();
+    }
+    render();
+  };
+
+  const render = () => {
+    const template = this.menu[this.currentCategory]
+      .map((menuItem, index) => {
+        return `
+    <li data-menu-id="${index}" class="menu-list-item d-flex items-center  py-2">
+          <span class="${
+            menuItem.soldOut ? "sold-out" : ""
+          } w-100 pl-2 menu-name">${menuItem.name}</span>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+          >
+            품절
+          </button>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+          >
+            수정
+          </button>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+          >
+            삭제
+          </button>
+    </li>`;
+      })
+      .join("");
+
+    // 메뉴아이템 리스트 추가
+    $("#menu-list").innerHTML = template;
+    updateMenuCount();
+  };
   const updateMenuCount = () => {
     // 메뉴아이템 갯수 반영 -> CLASS참조 ".클래스이름"
     // li태그가 몇개있는지 세기로 -> querySelectorAll("li") 태그 전부 세기
-    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    const menuCount = $("#menu-list").querySelectorAll("li").length;
     $(".menu-count").innerText = `총 ${menuCount}개`;
   };
 
   // 두가지 이벤트에서 재사용할 수 있는 함수 만들기
   const addMenuName = () => {
     // 사용자 입력값이 빈 값이라면 추가되지 않는다.
-    if ($("#espresso-menu-name").value === "") {
+    if ($("#menu-name").value === "") {
       alert("값을 입력해주세요."); // 여기까지만 하면 뒷부분이 실행되서 빈값이 목록에 추가됌
       // 해결하기 위해서는 return해주면됌
       return; // 뒷부분이 실행이 안됌!
     }
     // 인풋폼에 메뉴의 이름을 입력받고 추가한다.
-    const espressoMenuName = $("#espresso-menu-name").value;
-    this.menu.push({ name: espressoMenuName }); // 상태 배열값에 메뉴 추가
+    const menuName = $("#menu-name").value;
+    this.menu[this.currentCategory].push({ name: menuName }); // 상태 배열값에 메뉴 추가
     store.setLocalStorage(this.menu); // 상태변화가 되었을때 데이터 저장
-    const template = this.menu
-      .map((item, index) => {
-        return `
-  <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
-      <span class="w-100 pl-2 menu-name">${item.name}</span>
-      <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-       >
-       수정
-      </button>
-      <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-      >
-       삭제
-      </button>
-  </li>`;
-      })
-      .join("");
-
-    // 메뉴아이템 리스트 추가
-    $("#espresso-menu-list").innerHTML = template;
-    updateMenuCount();
+    render();
     // 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-    $("#espresso-menu-name").value = "";
+    $("#menu-name").value = "";
   };
 
   // 수정기능 함수
   const updateMenuName = (e) => {
-    const menuId = e.target.closest("li").dataset.menuId; // data-menu-id 속성
+    const menuId = e.target.closest("li").dataset.menuId; // data-menu-id 속성에 접근(.dataset.menuId)
     // e 객체를 받아서~
     // 타겟에 가장 가까운 부모 ".closest("li")" 로 가서
     // 그 안에있는 클래스 ".querySelector(".menu-name")"를 선택하는 방법
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     //.innerText -> 텍스트로 가져옴
     const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
-    this.menu[menuId].name = updatedMenuName;
+    this.menu[this.currentCategory][menuId].name = updatedMenuName;
     store.setLocalStorage(this.menu); // 로컬스토리지에 업데이트
     // prompt가 바뀐값을 리턴한다.
     $menuName.innerText = updatedMenuName;
@@ -143,40 +168,58 @@ function App() {
   // 삭제기능 함수
   const removeMenuName = (e) => {
     if (confirm("정말 삭제하시겠습니까?")) {
-      // e.target.closest("li") ->  li태그 안의 전부를 가져옴
-      // console.log(e.target.closest("li"));
+      const menuId = e.target.closest("li").dataset.menuId; // data-menu-id 속성에 접근(.dataset.menuId)
+      this.menu[this.currentCategory].splice(menuId, 1); // 삭제
+      store.setLocalStorage(this.menu); // 로컬스토리지에 업데이트
+      // TODO: 똑같이 했는데 왜 로컬스토리지에서는 삭제 안되고 가끔 다른게 삭제돼?
       e.target.closest("li").remove(); // 삭제
       updateMenuCount(); // 총 갯수 카운터에 반영
     }
   };
 
+  // 품절기능 함수
+  const soldOutMenu = (e) => {
+    const menuId = e.target.closest("li").dataset.menuId;
+    this.menu[this.currentCategory][menuId].soldOut =
+      !this.menu[this.currentCategory][menuId].soldOut;
+    store.setLocalStorage(this.menu);
+    render();
+  };
+
   // 이벤트 위임 -> https://blog.makerjun.com/5326e691-16cf-43f9-8908-00cc586f0884
-  $("#espresso-menu-list").addEventListener("click", (e) => {
+  $("#menu-list").addEventListener("click", (e) => {
     if (e.target.classList.contains("menu-edit-button")) {
       // 이벤트 타겟이 가지고 있는 클래스중에 menu-edit-button클래스가 포함되면
       // 수정버튼을 누른 경우만!
       updateMenuName(e); // e -> 이벤트 객체를 함수에 넘겨준다
+      return; // return을 붙이면 불필요한 아래의 연산을 할 필요가 없어진다(습관들이기)
     }
 
     if (e.target.classList.contains("menu-remove-button")) {
       // 삭제버튼을 누른 경우만!
       removeMenuName(e);
+      return;
+    }
+
+    if (e.target.classList.contains("menu-sold-out-button")) {
+      soldOutMenu(e);
+      return;
     }
   });
 
   // form태그가 자동으로 전송되는걸 막아준다.
   // -> form태그 때문에 엔터키 입력시 자동으로 새로고침 되기 때문
-  $("#espresso-menu-form").addEventListener("submit", (e) => {
+  $("#menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
   });
 
   // 메뉴의 이름을 입력받는건 ID값 참조 "#아이디"
 
   // 확인버튼 이벤트
-  $("#espresso-menu-submit-button").addEventListener("click", addMenuName);
+  $("#menu-submit-button").addEventListener("click", addMenuName);
 
   // 엔터키 이벤트
-  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+  $("#menu-name").addEventListener("keypress", (e) => {
     if (e.key !== "Enter") {
       // enter키를 안눌렀을때 종료가되고
       // enter키를 누르면 실행되고
@@ -184,5 +227,19 @@ function App() {
     }
     addMenuName();
   });
+
+  // 카테고리 메뉴 선택시 바뀌게
+  $("nav").addEventListener("click", (e) => {
+    const isCategoryButton = e.target.classList.contains("cafe-category-name");
+
+    if (isCategoryButton) {
+      // html 속성 가져오기 ->  data-category-name="속성값"
+      const categoryName = e.target.dataset.categoryName;
+      this.currentCategory = categoryName;
+      $("#category-title").innerText = `${e.target.innerText} 메뉴 관리`;
+      render();
+    }
+  });
 }
-new App(); // new 를 안쓰면 this.menu의 this 가 윈도우가 된다
+const app = new App(); // new 를 안쓰면 this.menu의 this 가 윈도우가 된다
+app.init();
